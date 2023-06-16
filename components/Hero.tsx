8565205a06a87d5003261525311ec721c280c46a
Bryan W <bhoff1980@gmail.com>
2023-06-15 22:27:23 -0600

    began with hero section but wanting to figure out main branch situation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,8 +3,10 @@ import Image from 'next/image'
 
 const Hero = () => {
   return (
-    <div>
-      Hero
+    <div className='hero'>
+      <div className='flex-1 pt-36 padding-x'>
+        
+      </div>
     </div>
   )
 }
