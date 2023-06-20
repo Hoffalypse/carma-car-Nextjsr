73518bf3bd8c626f045cab1fa2144d26e5c90d2c
Bryan W <bhoff1980@gmail.com>
2023-06-19 19:28:24 -0600

    started hero section and reusable button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
+import CustomButton from "./CustomButton";
 
 const Hero = () => {
   return (
-    <div>
-      Hero
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, Book, or Rent a car Quickly and Easily!
+        </h1>
+        <p className="hero__subtitle"> Streamline your car rental experience with our effortless process</p>
+        <CustomButton />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
