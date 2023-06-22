e392a1d8a1045b08af464434e790adc78bcec175
Bryan W <bhoff1980@gmail.com>
2023-06-22 14:16:56 -0600

    built algo to create an estimated rental price for each vehicle

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -8,9 +8,16 @@ interface CarCardProps {
 }
 
 const CarCard = ({car}: CarCardProps) => {
+
+  const { city_mpg, year, make, model, transmission, drive} = car;
   return (
-    <div>
-      carolla
+    <div className='car-card group'>
+      <div className='car-card__content'>
+        <h2 className='car-card__content-title'>{make} {model}</h2>
+      </div>
+      <p>
+        <span>Car Rent..</span>
+        </p>
     </div>
   )
 }
