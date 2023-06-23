3a2a3817829ee49f0073d221e836e786d6d2bb20
John <johnwilday@yahoo.com>
2023-06-22 21:07:47 -0600

    Changed car model

--- utils/index.ts
+++ utils/index.ts
@@ -5,7 +5,7 @@ export async function fetchCars() {
         'X-RapidAPI-Key': '2ab020db8cmsh21324bfdaa7b502p1595a2jsnbe0be0637a64',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {headers:headers});
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera', {headers:headers});
     const result = await response.json()
 
     return result
@@ -26,3 +26,4 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   
     return rentalRatePerDay.toFixed(0);
   };
+
