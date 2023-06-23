9347b0cffa9a628d53ed686b1b2634f4a780f84c
John <johnwilday@yahoo.com>
2023-06-22 22:20:30 -0600

    Added to next config file

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -2,7 +2,7 @@
 import {useState} from 'react'
 import Image from 'next/image'
 import { CarProps } from '@/types'
-import { calculateCarRent } from '@/utils'
+import { calculateCarRent, generateCarImageUrl } from '@/utils'
 import CustomButton from './CustomButton'
 import CarDetails from './CarDetails'
 
@@ -28,7 +28,7 @@ const CarCard = ({car}: CarCardProps) => {
       <span className='self-end text-[12px] font-medium'>per day</span>
         </p>
         <div className='relative w-full h-40 my-3 object-contain'>
-          <Image src='/hero.png' fill priority alt='car model' className='object-contain' />
+          <Image src={generateCarImageUrl(car)} fill priority alt='car model' className='object-contain' />
         </div>
         <div className='relative flex w-full mt-2'>
           <div className='flex group-hover:invisible w-full justify-between text-gray'>
